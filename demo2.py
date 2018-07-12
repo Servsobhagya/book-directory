e8743dd3a7b53cc3fc0b2485e370d3d27fc6071d
Servsobhagya <33262637+Servsobhagya@users.noreply.github.com>
2018-07-13 00:02:43 +0530

    Delete demo2.py

--- demo2.py
+++
@@ -1,7 +0,0 @@
-import re
-
-email1= "zuck26@facebook.com"   "page33@google.com"  "jeff42@amazon.com"
-p=re.compile(r"([a-zA-Z0-9._]+)@([a-zA_Z]+).([com)]+)")
-result1=p.findall(email1)
-
-print(result1)
